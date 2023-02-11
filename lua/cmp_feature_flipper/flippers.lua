700c7dda1925178d0e2ff521be15f0e1f796e777
Wassim Metallaoui <wassim@metallaoui.com>
2023-02-11 13:34:16 -0600

    fix: add matching for Feature.enabled?

--- lua/cmp_feature_flipper/flippers.lua
+++ lua/cmp_feature_flipper/flippers.lua
@@ -42,11 +42,12 @@ function M.valid_prefix(prefix)
   end
 
   local valid_prefixes = {
-    "feature_enabled?",
-    "with_feature",
-    "without_feature",
-    "featureEnabled",
-    "Features.feature_enabled?",
+    'feature_enabled?',
+    'with_feature',
+    'without_feature',
+    'featureEnabled',
+    'Features.feature_enabled?',
+    'Features.enabled?',
   }
 
   local match = false
